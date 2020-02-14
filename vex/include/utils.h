4e8904efaead76dcefb95c8c1f01979480be1a00
Joseph Chow <josephchow@fb.com>
2020-02-13 16:32:42 -0800

    Update the comments on the utility functions as well as in-project.

--- vex/include/utils.h
+++ vex/include/utils.h
@@ -1,18 +1,23 @@
+/**
+ 
+    This is a series of helpers functions for use within VEX.
+    These are all fairly comon functions but grabbed from a variety of areas on the interwebs.
+ */
 
 
-// sets magnitude for a vector
+// returns the magnitude for a vector
 vector setMag(vector vec; float max){
     vec = normalize(vec);
     vec *= max;
     return vec;
 }
 
-
-
+// returns the magnitude of a vector squared.
 float magSq(vector vec){
     return vec.x * vec.x + vec.y * vec.y + vec.z * vec.z;
 }
 
+// constrains a vector to not be above a certain value.
 vector limit(vector vec; float max){
     float mSq = magSq(vec);
     if(mSq > max * max){
@@ -21,9 +26,7 @@ vector limit(vector vec; float max){
     }
     
     return vec;
-    
 }
-
 
 /**
     Generates a random float value between a min and a max.
@@ -33,24 +36,29 @@ float randFloat(float seed; int min; int max){
     return fit(rand(seed),0,1,min,max);
 }
 
-
+// rotate a matrix along the x axis at a certain angle.
 matrix3 rotateX(matrix3 mat; float angle){
     vector axis = {1,0,0};
     return rotate(mat, angle, axis);
 }
 
+// rotate a matrix along the y axis at a certain angle.
 matrix3 rotateY(matrix3 mat; float angle){
     vector axis = {0,1,0};
     return rotate(mat, angle, axis);
 }
 
+// rotates a matrix along the z axis at a certain angle.
 matrix3 rotateZ(matrix3 mat; float angle){
     vector axis = {0,0,1};
     return rotate(mat, angle, axis);
 }
 
-// does a rotation on all three axis 
-matrix3 randomRotation(matrix3 mat; float angle){
+/**
+    Does a random rotation using matrices.
+    Pass in a min and a max to use for the seedingi value.
+ */
+matrix3 randomRotation(int min;int max){
     vector axisX = {1,0,0};
     vector axisY = {0,1,0};
     vector axisZ = {0,0,1};
@@ -59,12 +67,27 @@ matrix3 randomRotation(matrix3 mat; float angle){
     matrix3 yMat = ident();
     matrix3 zMat = ident();
     
-    
+    float angle = randFloat(3.14 * i,min,max);
     rotate(xMat,angle,axisX);
     rotate(yMat,angle,axisY);
     rotate(zMat,angle,axisZ);
     
-    
     return xMat * yMat * zMat;
+    
+}
+
+/**
+    Basically achieves the same thing as randomRotation but through the use of
+    quaternions. Pass in the seed value that you want to change.
+ */
+vector quatRotateRandom(vector angles; vector seed){
+    vector rotations = set(
+        randFloat(3.14 * i,1,2),
+        randFloat(3.14 * i,1,3),
+        randFloat(3.14 * i,1,4)
+             
+    );
+    
+    return qrotate(quaternion(rotations),seed);
     
 }
